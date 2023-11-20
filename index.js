00fc7e59536f2aa56695778b98c3aa333f997744
jsmld <samvanerpersroijaards@gmail.com>
2023-11-19 21:59:13 -0300

    Added collision for square

--- index.js
+++ index.js
@@ -7,16 +7,25 @@ let y = canvas.height - 55
 let dx = 2
 let dy = -2
 
+const squareSize = 50
+
 function drawSquare() {
   ctx.beginPath()
   ctx.fillStyle = "orange"
-  ctx.fillRect(x,y,50,50)
+  ctx.fillRect(x, y, squareSize, squareSize)
   ctx.closePath()
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawSquare()
+
+  if(x + dx > canvas.width - squareSize || x + dx < 0) {
+    dx = -dx
+  }
+  if(y + dy > canvas.height - squareSize || y + dy < 0) {
+    dy = -dy
+  }
   x += dx
   y += dy
 }
