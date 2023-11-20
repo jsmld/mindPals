cb59fcc7ccce2839df49ad9de79f90d64ff54488
jsmld <samvanerpersroijaards@gmail.com>
2023-11-19 21:48:02 -0300

    Self moving square

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
-ctx.fillStyle = "orange"
-ctx.fillRect(100,100,50,50)
+let x = canvas.width / 2
+let y = canvas.height - 55
+
+let dx = 2
+let dy = -2
+
+function drawSquare() {
+  ctx.beginPath()
+  ctx.fillStyle = "orange"
+  ctx.fillRect(x,y,50,50)
+  ctx.closePath()
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawSquare()
+  x += dx
+  y += dy
+}
+
+setInterval(draw, 10)
